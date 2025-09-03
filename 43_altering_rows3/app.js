26e4ef0924076d83faa64b06187ceb6fad8e419b
SUHO PARK <duglikespiano@gmail.com>
2025-09-04 00:50:14 +0900

    43 - ALTERING ROWS3

---
+++ 43_altering_rows3/app.js
@@ -0,0 +1,60 @@
+const sections = gsap.utils.toArray('.cols-2');
+
+// important to do this BEFORE ScrollTriggers are created
+// saveStyles stores initial styles of selected DOM
+// if the styles are not stored before ScrollTriggers are created, the styles will be broken by resizing the viewport from the breakpoint and the other breakpoint
+ScrollTrigger.saveStyles('.item, .item div');
+
+ScrollTrigger.matchMedia({
+	// desktop
+	'(min-width: 601px)': function () {
+		sections.forEach((element, index) => {
+			let items = element.querySelectorAll('.item');
+			let distance = index % 2 == 0 ? 100 : -100;
+			let tl = gsap.timeline({ paused: true });
+
+			tl.from(items, { duration: 0.5, xPercent: gsap.utils.wrap([-distance, distance]) });
+			tl.from(items, { opacity: 0, duration: 0.3 }, 0);
+
+			ScrollTrigger.create({
+				trigger: element,
+				start: 'top 80%',
+				onEnter: () => tl.play(),
+			});
+
+			ScrollTrigger.create({
+				trigger: element,
+				start: 'top 100%',
+				onLeaveBack: () => tl.pause(0),
+			});
+		});
+	},
+
+	// small screen
+	'(max-width: 600px)': function () {
+		// Any ScrollTriggers created inside these functions are segregated and get
+		// reverted/killed when the media query doesn't match anymore.
+		gsap.utils.toArray('.item').forEach((element, index) => {
+			let target = element.querySelector('div');
+
+			let tl = gsap.timeline({ paused: true });
+			if (target.classList.contains('photo')) {
+				tl.from(target, { duration: 0.5, opacity: 0, scale: 0.8 });
+			} else {
+				tl.from(target, { duration: 0.5, opacity: 0, y: 100 });
+			}
+
+			ScrollTrigger.create({
+				trigger: element,
+				start: 'top 90%',
+				onEnter: () => tl.play(),
+			});
+
+			ScrollTrigger.create({
+				trigger: element,
+				start: 'top 100%',
+				onLeaveBack: () => tl.pause(0),
+			});
+		});
+	},
+});
