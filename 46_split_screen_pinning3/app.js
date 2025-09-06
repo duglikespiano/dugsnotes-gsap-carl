b2f249fa93119a976124edf4865bdcef8ed15765
SUHO PARK <duglikespiano@gmail.com>
2025-09-07 00:35:59 +0900

    46 - SPLIT SCREEN PINNING3

---
+++ 46_split_screen_pinning3/app.js
@@ -0,0 +1,24 @@
+const details = gsap.utils.toArray('.desktopContentSection:not(:first-child)');
+
+const photos = gsap.utils.toArray('.desktopPhoto:not(:first-child)');
+
+gsap.set(photos, { yPercent: 101 });
+
+ScrollTrigger.create({
+	trigger: '.gallery',
+	start: 'top top',
+	end: 'bottom bottom',
+	pin: '.right',
+});
+
+details.forEach((detail, index) => {
+	let headline = detail.querySelector('h1');
+	ScrollTrigger.create({
+		trigger: headline,
+		start: 'top 80%',
+		end: 'top 50%',
+		animation: gsap.to(photos[index], { yPercent: 0 }),
+		scrub: true,
+		markers: false,
+	});
+});
